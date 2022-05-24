5447f73440b63eac98afe293f78ae4be92f430ac
Aleksandr <92846541+AleksandrZelukin@users.noreply.github.com>
2022-05-24 07:44:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import *
+from sienas import *
+from saulite import *
+from durvis import *
+from lodzins import *
+from dumenis import *
+from jumta import *
+
+setup(800,600)
+shape("turtle")
+pencolor("yellow")
+fillcolor("red")
+
+
+
+done()
